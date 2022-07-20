05b457edc332f0d7b3e56d864db2d53226dca95f
Tarcisio Miranda <eu@tarcisio.me>
2022-07-20 20:18:29 -0300

    work api

--- src/main/java/tarcisio/me/springboot/controllers/UserController.java
+++ src/main/java/tarcisio/me/springboot/controllers/UserController.java
@@ -1,8 +1,13 @@
 package tarcisio.me.springboot.controllers;
 
-// import org.apache.catalina.User;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +17,9 @@ import tarcisio.me.springboot.model.User;
 @RequestMapping("/users")
 public class UserController {
 
+    private List<User> users = new ArrayList<>();
+
+    // GET
     @GetMapping("/")
     public User user() {
         User user = new User();
@@ -24,6 +32,7 @@ public class UserController {
 
     @PostMapping("/") 
     public User user(@RequestBody User user) {
+        users.add(user);
         return user;
     }
 
--- src/main/java/tarcisio/me/springboot/model/User.java
+++ src/main/java/tarcisio/me/springboot/model/User.java
@@ -10,6 +10,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+
     private Long id;
     private String name;
     private String username;
