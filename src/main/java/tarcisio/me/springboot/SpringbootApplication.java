9d1f9c0dda5c3bc1e54b6f27623da9480c082e05
Tarcisio Miranda <eu@tarcisio.me>
2022-07-28 11:04:36 -0300

    terminando parte do banco

--- src/main/java/tarcisio/me/springboot/SpringbootApplication.java
+++ src/main/java/tarcisio/me/springboot/SpringbootApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootApplication {
-
+	
+	// @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
