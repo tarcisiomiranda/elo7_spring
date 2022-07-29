72d4e9516eb2d4083400ea5b54f4195cf7ad875a
Tarcisio Miranda <eu@tarcisio.me>
2022-07-29 19:01:43 -0300

    Removing unnecessary comments and spaces 2

--- src/main/java/tarcisio/me/springboot/model/Sonda.java
+++ src/main/java/tarcisio/me/springboot/model/Sonda.java
@@ -17,16 +17,15 @@ import lombok.Setter;
 @Entity
 @Table(name = "sondas")
 public class Sonda {
-    
-    // @GeneratedValue(strategy =GenerationType.AUTO)
+
     @Id
     @ApiModelProperty(value = "id = Número da sonda", example = "1")
     private Long id;
     @ApiModelProperty(value = "name = Nome da sonda", example = "sonda1")
     private String name;
-    @ApiModelProperty(value = "eixo X que vai de 1 a 5", example = "x=3")
+    @ApiModelProperty(value = "eixo X que vai de 1 a 5", example = "3")
     private int x;
-    @ApiModelProperty(value = "eixo Y que vai de 1 a 5", example = "y=1")
+    @ApiModelProperty(value = "eixo Y que vai de 1 a 5", example = "1")
     private int y;
     @ApiModelProperty(value = "Posição da sonda como: N S E W", example = "W")
     private String posicao;
